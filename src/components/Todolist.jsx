a97682a5a89649d31647af06c8bdee6d13c4fdbf
farhatbenabdessamad <farhat.benabdessamad@fsegt.utm.tn>
2024-03-11 10:34:39 +0100

    first

---
+++ src/components/Todolist.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { delete_task } from '../redux/action'
+import Edittask from './Edittask'
+import { useState } from 'react'
+
+function Todolist() {
+    const todo=useSelector(state=>state.todo)
+    const dispatch=useDispatch()
+    console.log(todo)
+    const [iscomplete, setIscomplete]=useState(false)
+  return (
+    <div>
+      <Button onClick={()=>setIscomplete(true)}>iscomplete</Button>
+      <Button onClick={()=>setIscomplete(false)}>Notcomplete</Button>
+       {todo.filter(e=>e.iscomplete===iscomplete).map(e=>
+        <Card style={{width:"18rem",backgroundColor:"green"}}>
+            <Card.Body>
+                <Card.Text>{e.task}</Card.Text>
+                <Card.Text>{e.time}</Card.Text>
+            <input type="checkbox" checked={e.iscomplete} />
+            <Button variant='danger' onClick={()=>dispatch(delete_task(e.id))}>delete</Button>
+            <Edittask tasks={e}/>
+            </Card.Body>
+        </Card>
+        )} 
+    </div>
+  )
+}
+
+export default Todolist
